3153da69f0b3bd18fb67d9581775e5c3500f9dde
Artiom Nichifor <Artiom Nichifor>
2017-05-26 00:15:12 +0300

    i:wqq#

---
+++ lab 5/App/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText nameTxt, phoneTxt, emailTxt, addressTxt;
+    ImageView contactImageView;
+    List<Contact> Contacts = new ArrayList<Contact>();
+    ListView contactListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameTxt = (EditText) findViewById(R.id.txtName);
+        phoneTxt = (EditText) findViewById(R.id.txtPhone);
+        emailTxt = (EditText) findViewById(R.id.txtEmail);
+        addressTxt = (EditText) findViewById(R.id.txtAddress);
+        contactListView = (ListView) findViewById(R.id.listView);
+        contactImageView = (ImageView) findViewById(R.id.imgViewContactImage);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("creator");
+        tabSpec.setContent(R.id.tabCreator);
+        tabSpec.setIndicator("Creator");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("list");
+        tabSpec.setContent(R.id.tabContactList);
+        tabSpec.setIndicator("List");
+        tabHost.addTab(tabSpec);
+
+        final Button addBtn = (Button) findViewById(R.id.btnAdd);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"Your contact was created", Toast.LENGTH_SHORT).show();
+                addContacts(nameTxt.getText().toString(), phoneTxt.getText().toString(), emailTxt.getText().toString(), addressTxt.getText().toString());
+                populateList();
+                Toast.makeText(getApplicationContext(), nameTxt.getText().toString() + "has been aded to your Contacts", Toast.LENGTH_SHORT).show();
+        }
+        });
+
+        nameTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                addBtn.setEnabled(!nameTxt.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        contactImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Content Image"), 1);
+            }
+        });
+    }
+
+    public void onActivityResult(int reqCode, int resCode, Intent data){
+        if (resCode == RESULT_OK) {
+            if (reqCode == 1)
+                contactImageView.setImageURI(data.getData());
+        }
+    }
+
+    private  void populateList(){
+        ArrayAdapter<Contact> adapter = new ContactListAdapter();
+        contactListView.setAdapter(adapter);
+    }
+
+    private void addContacts(String name, String phone, String email, String address){
+        Contacts.add(new Contact(name, phone, email, address));
+
+    }
+
+
+    private class ContactListAdapter extends ArrayAdapter<Contact>{
+        public ContactListAdapter(){
+            super(MainActivity.this, R.layout.listview_item, Contacts);
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent){
+            if (view == null)
+                view = getLayoutInflater().inflate(R.layout.listview_item, parent, false);
+
+            Contact currentContact = Contacts.get(position);
+
+            TextView name = (TextView) view.findViewById(R.id.contactName);
+            name.setText(currentContact.getName());
+            TextView phone = (TextView) view.findViewById(R.id.phoneNumber);
+            phone.setText(currentContact.getPhone());
+            TextView email = (TextView) view.findViewById(R.id.emailAddress);
+            email.setText(currentContact.getEmail());
+            TextView address = (TextView) view.findViewById(R.id.cAddress);
+            address.setText(currentContact.getAddress());
+
+            return view;
+        }
+    }
+
+
+
+}
+
